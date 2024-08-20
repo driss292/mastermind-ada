4cda5f9b20b1ea5fee78d611d657a837f2b13fe6
driss292 <drisskaci@outlook.fr>
2024-08-20 15:12:50 +0200

    WIP

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-let items = [...document.querySelectorAll(".player__box--item")];
+let boxItems = document.querySelector(".player__box");
+let items = [...document.querySelectorAll(".player__item")];
+
+items.forEach((item) => {
+  item.addEventListener("dragstart", () => {
+    setTimeout(() => item.classList.add("dragging"), 0);
+  });
+  item.addEventListener("dragend", () => {
+    item.classList.remove("dragging");
+  });
+});
+
+boxItems.addEventListener("dragover", (e) => {
+  e.preventDefault();
+  const draggingItem = document.querySelector(".dragging");
+  const siblings = [
+    ...boxItems.querySelectorAll(".player__item:not(.dragging)"),
+  ];
+  let nextSibling = siblings.find((sibling) => {
+    return e.clientY <= sibling.offsetTop + sibling.offsetHeight / 2;
+  });
+  boxItems.insertBefore(draggingItem, nextSibling);
+});
+
+boxItems.addEventListener("dragenter", (e) => e.preventDefault());
